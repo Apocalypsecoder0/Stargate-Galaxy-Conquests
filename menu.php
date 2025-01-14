51c82df4900510164d263436d79b555c92c035d8
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-14 12:17:55 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -11,16 +11,24 @@
         <h1>Welcome to Stargatewar Conquesst</h1>
         <nav>
             <ul>
-              <li><a href="inedex.php.php">Settings</a></li>
+
+                li><a href="empirecommandcenter.php.php">Empire Command Center</a></li>
+              <li><a href="inedex.php.php">Index</a></li>
                 <li><a href="home.php">Home</a></li>
+                  <li><a href="attack.php">attack</a></li>
+                   <li><a href="battle_log.php">Battle Log</a></li>
+                
                 <li><a href="fleet.php">Fleet</a></li>
                 <li><a href="resources.php">Resources</a></li>
                 <li><a href="research.php">Research</a></li>
-                <li><a href="technology.php">Settings</a></li>
+                <li><a href="technology.php">Technology</a></li>
 
-              <li><a href="all.php">Settings</a></li>
+               <li><a href="alliance.php">Alliance</a></li>
+                
+                <li><a href="inbox_mail.php">Ibox_Mail</a></li>
+                
                 <li><a href="settings.php">Settings</a></li>
-                <li><a href="options.php">Settings</a></li>
+                <li><a href="options.php">Options</a></li>
                 <li><a href="logout.php">Logout</a></li>
             </ul>
         </nav>
